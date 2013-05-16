5ba3eafad46c1c1deb6efa610cc19f2f47c3cc69
funui-HP-PD <funui@live.com>
2013-05-16 08:33:35 +0800

    changed http_path of compass.rb

--- config/compass.rb
+++ config/compass.rb
@@ -4,7 +4,7 @@ require 'compass-normalize'
 require 'ninesixty'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "../../"
 css_dir = "app/styles"
 sass_dir = "app/styles"
 images_dir = "app/images"
